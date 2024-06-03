68fbcfd10295c49a6ca25fa5e0563f822436d0f4
laboratorio <laboratorio@UPEAULA-31718>
2024-06-03 07:18:18 -0500

    Agregar archivos de proyecto.

---
+++ ExamenProgresoII/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamenProgresoII
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExamenProgresoII/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ExamenProgresoII
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
